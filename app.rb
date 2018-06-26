e7d90f173030c936d8da98c8df9151cfa3c453e2
Kennedy Morganfield <kennedymorganfield@gmail.com>
2018-06-26 16:35:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
-require_relative 'config/environment'
+require 'sinatra'
+
+# require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do #creating a route to the homepage
+    "Hello, World!"
+  end 
+  
+  get '/name' do
+    "My name is Kennedy"
+  end
+  
+  get '/hometown' do
+    "My hometown is St. Louis"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Nice For What"
+  end
+  
 end
